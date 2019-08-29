475562e5f822ff97745e4f9af52eb128a9d11e53
Skyproject12 <Skyproject 12>
2019-08-29 19:27:58 +0700

    onclick list view

---
+++ app/src/main/java/com/example/listview/HeroAdapter.java
@@ -0,0 +1,69 @@
+package com.example.listview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class HeroAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Hero> heroes;
+
+    public HeroAdapter(Context content) {
+        this.context = content;
+        this.heroes = heroes;
+    }
+
+    // to set array list heroes in main
+    public void setHeroes(ArrayList<Hero> heroes) {
+        this.heroes = heroes;
+    }
+
+    @Override
+    public int getCount() {
+        return heroes.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return heroes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    // set item for list vview
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView= LayoutInflater.from(context).inflate(R.layout.itemhero, parent , false);
+        }
+        ViewHolder viewHolder= new ViewHolder(convertView);
+        Hero hero= (Hero)getItem(position);
+        viewHolder.bind(hero);
+        return convertView;
+    }
+    // set view holder for list view
+    private class ViewHolder{
+        private TextView txName;
+        private TextView txtDescription;
+        private ImageView imgPhooto;
+        ViewHolder(View view){
+            txName= view.findViewById(R.id.txt_name);
+            txtDescription= view.findViewById(R.id.txt_description);
+            imgPhooto= view.findViewById(R.id.img_photo);
+        }
+        void bind(Hero hero){
+            txName.setText(hero.getName());
+            txtDescription.setText(hero.getDescription());
+            imgPhooto.setImageResource(hero.getPhoto());
+        }
+    }
+}
--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.listview;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] dataName;
+    private TypedArray dataPhoto;
+    private String[] dataDescription;
+    private HeroAdapter adapter;
+    private ArrayList<Hero> heroes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // cal array list data name
+        adapter= new HeroAdapter(this);
+        // check default data list view
+        //ArrayAdapter<String>adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+        ListView listView= findViewById(R.id.lv_list);
+        listView.setAdapter(adapter);
+        addItem();
+        Prepare();
+        // make onclick in lisview listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, heroes.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    // menampung semua file dalam array lalu dimasukkan ke dalam array list
+    private void addItem(){
+        heroes= new ArrayList<>();
+        for (int i = 0; i <dataName.length ; i++) {
+            Hero hero= new Hero();
+            //default image -1
+            hero.setPhoto(dataPhoto.getResourceId(i,-1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+        adapter.setHeroes(heroes);
+    }
+    private void Prepare(){
+        dataName= getResources().getStringArray(R.array.data_name);
+        dataDescription= getResources().getStringArray(R.array.data_description);
+        dataPhoto= getResources().obtainTypedArray(R.array.data_photo);
     }
 }
